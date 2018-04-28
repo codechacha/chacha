4a970525a14f427608ea1d9d1a8f3ef22c360570
Jeongsik Mun <farfs.dev@gmail.com>
2018-04-28 10:16:01 +0900

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var del = require('del');
 gulp.task('resize', function () {
     return gulp.src('images/*.*')
         .pipe(imageResize({
-            width: 512,
+            width: 1024,
             imageMagick: true
         }))
         .pipe(gulp.dest('images/fulls'))
